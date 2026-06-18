fa53d70123daea18bba6e155e05483841b7a5d6b
Pauline Ogrodzki <pauline-ogrodzki-frx@users.noreply.github.com>
2026-06-18 17:10:46 +0400

    Redirect root URL to report dashboard

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("platform_dashboard"), name="home"),
     path("admin/", admin.site.urls),
-    path(
-        "reports/",
-        include("reports.urls"),
-    ),
-    path(
-        "knowledge/",
-        include("knowledge.urls"),
-    ),
+    path("reports/", include("reports.urls")),
+    path("knowledge/", include("knowledge.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
-
